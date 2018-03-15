66939833bdf942a0a172910ab7815d4f7e58fdd5
Andrea Stagi <stagi.andrea@gmail.com>
2018-03-15 11:33:08 +0100

    Add additional parameters for unicornprinter

--- mylib/tests/test_unicornprinter.py
+++ mylib/tests/test_unicornprinter.py
@@ -2,3 +2,6 @@ from mylib.unicornprinter import uniformat
 
 def test_uniformat():
     assert uniformat('Hello everyone!') == '🦄 Hello everyone!'
+
+def test_uniformat_error():
+    assert uniformat('Something went wrong!', 'E') == '🚨 Something went wrong!'
