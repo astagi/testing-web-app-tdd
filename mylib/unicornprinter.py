f6089346ab88048b4317272bf360c2fe4e298b4f
Andrea Stagi <stagi.andrea@gmail.com>
2018-03-15 11:47:39 +0100

    Refactoring unicornprinter 🦄

--- mylib/unicornprinter.py
+++ mylib/unicornprinter.py
@@ -1,8 +1,11 @@
 
+ERR  = 'E'
+WARN = 'W'
+
 def uniformat(message, msg_type=None):
-    if msg_type == 'E':
+    if msg_type == ERR:
         return '🚨 {0}'.format(message)
-    elif msg_type == 'W':
+    elif msg_type == WARN:
         return '⚠️ {0}'.format(message)
     else:
         return '🦄 {0}'.format(message)
