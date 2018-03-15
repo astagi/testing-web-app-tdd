12c8479dd5643a526e96011c1d0fb9f90591fcad
Andrea Stagi <stagi.andrea@gmail.com>
2018-03-15 11:37:46 +0100

    Add warning statement (coverage dec.)

--- mylib/unicornprinter.py
+++ mylib/unicornprinter.py
@@ -2,5 +2,7 @@
 def uniformat(message, msg_type=None):
     if msg_type == 'E':
         return '🚨 {0}'.format(message)
+    elif msg_type == 'W':
+        return '⚠️ {0}'.format(message)
     else:
         return '🦄 {0}'.format(message)
