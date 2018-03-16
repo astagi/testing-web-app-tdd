73476776374aeef40525d9016dcecdaace4fd1ad
Andrea Stagi <stagi.andrea@gmail.com>
2018-03-16 16:56:53 +0100

    Add functional test with mocking

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -3,15 +3,23 @@ import time
 from django.urls import reverse
 
 
-def test_page_api_integration(live_server, client, selenium):
-    print (client.__dict__)
+def test_page_api_integration(live_server, client, selenium, mocker):
     selenium.get(live_server.url)
     selenium.maximize_window()
+    mocked_get_weather = mocker.patch('api.views.get_temperature_by_city')
+    mocked_get_weather.return_value = {
+        'temp' : 100,
+        'pressure' : 40,
+        'humidity' : 50,
+        'temp_min' : 30,
+        'temp_max' : 40,
+    }
     cityText = selenium.find_element_by_id('inputCity')
     cityText.send_keys('Pistoia')
     startSearchButton = selenium.find_element_by_id('startSearch')
     startSearchButton.click()
     time.sleep(5)
     tempResultLabel = selenium.find_element_by_id('tempResults')
+    assert tempResultLabel.text == 'In Pistoia there are 100°F'
     time.sleep(5)
 
